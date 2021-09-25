cadd1c0d472815de2c27abaaadf942f76d3f165b
comeonandroid <comeonandroid@gmail.com>
2021-09-25 17:04:58 +0300

    cards placeholder

--- src/elements/Card.js
+++ src/elements/Card.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from '@emotion/styled'
 
-function Card(props) {
+import { Flex } from '@chakra-ui/react'
+
+function Card({children, ...props}) {
   return (
-    <div>
-      
-    </div>
+    <Element
+      width='340px'
+      height='425px'
+    >
+      { children }
+    </Element>
   )
 }
 
 Card.propTypes = {
 
 }
+
+const Element = styled(Flex)`
+  position: relative;
+  background: rgb(13, 14, 8);
+  box-shadow: rgb(0 0 0 / 1%) 0px 0px 1px, rgb(0 0 0 / 4%) 0px 4px 8px, rgb(0 0 0 / 4%) 0px 16px 24px, rgb(0 0 0 / 1%) 0px 24px 32px;
+  border-radius: 24px;
+  border: 1px solid rgb(25, 28, 34);
+`
 
 export default Card
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,12 +1,46 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { Grid, Flex, Heading } from '@chakra-ui/react'
+
 import { Header, PageWrapper } from 'modules/layout'
+import { Card } from 'elements'
 
 function MainPage(props) {
   return (
     <PageWrapper>
       <Header/>
+      <Flex
+        flexDirection='column'
+        justifyContent='center'
+        h='100vh'
+        width='80%'
+        padding='0px 0px 128px 0px'
+        margin='0 auto'
+      >
+        <Heading as="h3" size="lg" padding='0px 0px 28px 6px' color='white'>Latest Releases</Heading>
+        <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+          <Card widht="100%" />
+          <Card widht="100%" />
+          <Card widht="100%" />
+          <Card widht="100%" />
+        </Grid>
+      </Flex>
+
+      {/* <Wrap padding='32px 24px' spacing="24px">
+        <WrapItem>
+          <Card />
+        </WrapItem>
+        <WrapItem>
+          <Card />
+        </WrapItem>
+        <WrapItem>
+          <Card />
+        </WrapItem>
+        <WrapItem>
+          <Card />
+        </WrapItem>
+      </Wrap> */}
     </PageWrapper>
   )
 }
