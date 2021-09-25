0f27cc4a5e189853607b9021495adc2f1bb987a1
comeonandroid <comeonandroid@gmail.com>
2021-09-25 17:33:13 +0300

    add link to main

--- src/modules/layout/Header.js
+++ src/modules/layout/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { useDisclosure, Flex, Button } from '@chakra-ui/react'
+import { useDisclosure, Flex, Button, Box, Heading } from '@chakra-ui/react'
 import { ConnectButton, AccountModal } from 'modules/wallet'
 import { Link } from 'react-router-dom'
 
@@ -11,17 +11,24 @@ function Header(props) {
   return (
     <Flex 
       flexDirection='row'
-      justifyContent='flex-end'
+      justifyContent='space-between'
       width='100%'
       padding='20px'
       alignItems='center'>
-      <Link to='/plans'>
-        <Button mr='18px' backgroundColor='pink.700' colorScheme="pink" variant="solid">
-          Get Premium
-        </Button>
-      </Link>
-      <ConnectButton handleOpenModal={onOpen} />
-      <AccountModal isOpen={isOpen} onClose={onClose} />
+      <Flex ml='12px'>
+        <Link to='/'>
+          <Heading as="h4" size="md" color='gray.200'>DemoApp</Heading>
+        </Link>
+      </Flex>
+      <Flex flexDirection='row'>
+        <Link to='/plans'>
+          <Button mr='18px' backgroundColor='pink.700' colorScheme="pink" variant="solid">
+            Get Premium
+          </Button>
+        </Link>
+        <ConnectButton handleOpenModal={onOpen} />
+        <AccountModal isOpen={isOpen} onClose={onClose} />
+      </Flex>
     </Flex>
   )
 }
