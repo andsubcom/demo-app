d7a195a4d2d0fb7786fdc081c7a1d99b68637ec8
comeonandroid <comeonandroid@gmail.com>
2021-09-25 17:10:13 +0300

    change back

--- src/modules/layout/PageWrapper.js
+++ src/modules/layout/PageWrapper.js
@@ -5,11 +5,12 @@ import { Flex } from '@chakra-ui/react'
 
 function PageWrapper({children, ...rest}) {
   return (
-    <Flex 
+    <Flex
+      position='relative'
       flexDirection='column'
       h='100vh'
       w='100%'
-      bg='gray.900'
+      bg='rgb(33, 36, 41)'
       {...rest}
       >
       { children }
