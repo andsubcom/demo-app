0cfd183cf274203b7bc6fedd03f372a59da869bb
comeonandroid <comeonandroid@gmail.com>
2021-09-25 13:54:23 +0300

    Add loader

--- src/layouts/PrivateLayout.js
+++ src/layouts/PrivateLayout.js
@@ -2,9 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import { withRouter } from 'react-router-dom'
+import { Flex } from '@chakra-ui/react'
 
 import { useEthers } from '@usedapp/core'
 import { useAddressAssets } from 'defi-sdk'
+
+import { Loader } from 'elements'
 
 function PrivateLayout({ children }) {
   const { account } = useEthers()
@@ -22,9 +25,15 @@ function PrivateLayout({ children }) {
   console.log('assets', assets)
   console.log('account', account)
   return (
-    <>
-      { children }
-    </>
+    <Flex
+      flexDirection='column'
+      alignItems='center'
+      justifyContent='center'
+      h='100vh'
+      bg='gray.800'
+      >
+      <Loader/>
+   </Flex>
   )
 }
 
